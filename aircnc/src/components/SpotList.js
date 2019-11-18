cc59d0ea99b2e18ac2c1b77654791a32fc84ad04
ribeiromatheus <ribeiro.matheuspereiraribeiro>
2019-11-18 15:05:39 -0300

    feat: booking page

--- aircnc/src/components/SpotList.js
+++ aircnc/src/components/SpotList.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { withNavigation } from 'react-navigation'
 import { View, Text, StyleSheet, FlatList, Image, TouchableOpacity } from 'react-native';
 import api from '../services/api';
 
-export default function SpotList({ tech }) {
+function SpotList({ tech, navigation }) {
     const [spots, setSpots] = useState([]);
 
     useEffect(() => {
@@ -16,6 +17,10 @@ export default function SpotList({ tech }) {
 
         loadSpots();
     }, []);
+
+    function handleNavigate(id) {
+        navigation.navigate('Book', { id });
+    }
 
     return (
         <View style={styles.container}>
@@ -33,7 +38,7 @@ export default function SpotList({ tech }) {
                         <Image style={styles.thumbnail} source={{ uri: item.thumbnail_url }} />
                         <Text style={styles.company}>{item.company}</Text>
                         <Text style={styles.price}>{item.price ? `R$${item.price}/dia` : 'GRATUITO'}</Text>
-                        <TouchableOpacity onPress={() => { }} style={styles.button}>
+                        <TouchableOpacity onPress={() => handleNavigate(item._id)} style={styles.button}>
                             <Text style={styles.buttonText}>Solicitar reserva</Text>
                         </TouchableOpacity>
                     </View>
@@ -93,3 +98,5 @@ const styles = StyleSheet.create({
         fontSize: 15
     }
 });
+
+export default withNavigation(SpotList);
